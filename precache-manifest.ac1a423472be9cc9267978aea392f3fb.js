65c516c76e26f765692562ddc27ff863a1f208a0
rahul7668gupta <rahul7668gupta@gmail.com>
2020-07-28 18:49:46 +0530

    Updates

---
+++ precache-manifest.ac1a423472be9cc9267978aea392f3fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2c13f9596db07a1d64ad74a1e80f85",
+    "url": "/rahul7668gupta/whistle-blower/index.html"
+  },
+  {
+    "revision": "4793756722eb8051b28c",
+    "url": "/rahul7668gupta/whistle-blower/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "302ccb083c4d4e713328",
+    "url": "/rahul7668gupta/whistle-blower/static/js/2.1828e4fd.chunk.js"
+  },
+  {
+    "revision": "4793756722eb8051b28c",
+    "url": "/rahul7668gupta/whistle-blower/static/js/main.d6575b9a.chunk.js"
+  },
+  {
+    "revision": "47fac1676fdf9466d71b",
+    "url": "/rahul7668gupta/whistle-blower/static/js/runtime-main.ca8c38e6.js"
+  }
+]);
