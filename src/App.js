701c04c72d5f5cc5e5856d16f5f1ee94a13f0b9e
rahul7668gupta <rahul7668gupta@gmail.com>
2020-07-28 18:50:54 +0530

    changed repo

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
 
       // Use web3 to get the user's accounts.
       this.accounts = await this.web3.eth.getAccounts();
-        console.log("Accounts from metamask: " + this.accounts);
+      console.log("Accounts from metamask: " + this.accounts);
 
       // Get the contract instance.
       this.networkId = await this.web3.eth.net.getId();
@@ -36,8 +36,8 @@ class App extends Component {
       this.whistleBlowerContract = new this.web3.eth.Contract(
         whistleBlower.abi,
         whistleBlower.networks[this.networkId] && whistleBlower.networks[this.networkId].address,
-        );
-        console.log("Contract Instance : " + this.whistleBlowerContract);
+      );
+      console.log("Contract Instance : " + this.whistleBlowerContract);
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
@@ -58,8 +58,81 @@ class App extends Component {
     }
   };
 
+  handleRegisterOrganisation = async() => {
+    let newOrg = await this.whistleBlowerContract.methods.registerOrganisation(
+      this.state.orgName,
+      this.state.orgType,
+      this.state.orgAddress,
+      this.state.url,
+      this.state.noOfOrgs
+    ).send({ from: this.accounts[0] })
+    console.log(newOrg);
+  }
 
+  getNoOfOrgs = async() => {
+    const noOfOrgs = await this.whistleBlowerContract.methods.organisationMappingLength;
+    this.setState({
+      noOfOrgs: noOfOrgs
+    })
+  }
 
+  getCaseCount = async() =>  {
+    const caseCount = await this.whistleBlowerContract.methods.caseMappingLength;
+    this.setState({
+      caseCount: caseCount
+    })
+  }
+
+  handleCaseMapping = async() =>  {
+    let caseArray = [];
+    for (let i = 0; i < this.state.caseCount; i++) {
+      let caseData = await this.whistleBlowerContract.methods.caseMapping(i).call();
+      caseArray.push(caseData);
+    }
+    this.setState({
+      caseArray: caseArray
+    })
+  }
+
+  handleOrganisationMapping = async() =>  {
+    let orgArray = [];
+    for (let i = 0; i < this.state.noOfOrgs; i++) {
+      let orgData = await this.whistleBlowerContract.methods.organisationMapping(i).call();
+      orgArray.push(orgData);
+    }
+    this.setState({
+      orgArray: orgArray
+    })
+  }
+
+  handleOrgCaseCount = async(address) => {
+    let thisOrgCaseCount = await this.whistleBlowerContract.methods.orgCaseCount(address).call();
+    console.log(thisOrgCaseCount);
+  }
+
+  handleOrgLogin = async (address) => {
+    return await this.whistleBlowerContract.methods.isAnOrganisationLogin(address).call();
+  }
+
+  handleCreateCase = async (address) => {
+    let newCase = await this.whistleBlowerContract.methods.createCase(
+      this.state.title,
+      this.state.description,
+      this.state.proof,
+      this.accounts[0],
+      this.state.url,
+      this.state.caseCount
+    ).send({ from: this.accounts[0] })
+    console.log(newCase);
+  }
+
+  handleApproveValidity = async (id) => {
+    let approved = await this.whistleBlowerContract.methods.approveValidity(id).send({
+      from: this.accounts[0]
+    });
+    console.log("Validity Increased: " + approved);
+  }
+  
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
